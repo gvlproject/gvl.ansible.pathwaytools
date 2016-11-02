77851f68960ca48b9e942bde2e7eaf838e9d97b6
Madison Flannery <madisonflannery93@gmail.com>
2016-11-02 14:56:54 +1100

    Added pathway tools install script

---
+++ install_pathway_tools.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#####################################################
+# script run on target machine to install Web Apollo
+#####################################################
+
+[ -d "/mnt/gvl/apps" ] || mkdir -p /mnt/gvl/apps # ensure /mnt/gvl/apps exists
+cd /opt/gvl
+if [ -d "/opt/gvl/gvl.ansible.web_apollo" ]; then
+  git pull https://github.com/gvlproject/gvl.ansible.pathwaytools
+else
+  git clone -b release_GVL_4.1 https://github.com/gvlproject/gvl.ansible.pathwaytools
+fi
+cd /opt/gvl/gvl.ansible.pathwaytools
+ansible-playbook playbook.yml
